d8f2babfe08b5fb7d582d7f75b0301f6ff04a3cc
Mike Hans <marthurhans@gmail.com>
2024-08-08 13:15:07 -0400

    start 09-java-config-bean - finished CH 57

--- 08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.luv2code.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,18 @@ public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In constructor: " + this.getClass().getSimpleName());
+    }
+
+    // define our init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + this.getClass().getSimpleName());
+    }
+
+    // define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + this.getClass().getSimpleName());
     }
 
     @Override
